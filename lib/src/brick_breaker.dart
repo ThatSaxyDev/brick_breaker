c80af978cc8d6ac233e9908202cfa26f2ac57584
Kiishi David <thatsaxydev@gmail.com>
2024-11-02 11:22:31 +0100

    comolete

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -2,12 +2,20 @@ import 'dart:async';
 import 'dart:math' as math;
 
 import 'package:brick_breaker/src/components/ball.dart';
+import 'package:brick_breaker/src/components/bat.dart';
+import 'package:brick_breaker/src/components/brick.dart';
 import 'package:brick_breaker/src/components/play_area.dart';
 import 'package:brick_breaker/src/config.dart';
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class BrickBreaker extends FlameGame with HasCollisionDetection {
+enum PlayState { welcome, playing, gameOver, won }
+
+class BrickBreaker extends FlameGame
+    with HasCollisionDetection, KeyboardEvents, TapDetector {
   BrickBreaker()
       : super(
             camera: CameraComponent.withFixedResolution(
@@ -18,6 +26,34 @@ class BrickBreaker extends FlameGame with HasCollisionDetection {
   double get width => size.x;
   double get height => size.y;
   final rand = math.Random();
+  final ValueNotifier<int> score = ValueNotifier(0);
+
+  late Bat bat;
+
+  late PlayState _playState; // Backing field for play state
+
+  PlayState get playState => _playState;
+
+  set playState(PlayState playState) {
+    _playState = playState;
+
+    // Handle overlays based on the new play state
+    switch (playState) {
+      case PlayState.welcome:
+        overlays.add(playState.name);
+      case PlayState.gameOver:
+        overlays.add(playState.name);
+      case PlayState.won:
+        // Add overlay for the current state
+        overlays.add(playState.name);
+
+      case PlayState.playing:
+        // Remove overlays for other states when playing
+        overlays.remove(PlayState.welcome.name);
+        overlays.remove(PlayState.gameOver.name);
+        overlays.remove(PlayState.won.name);
+    }
+  }
 
   @override
   FutureOr<void> onLoad() async {
@@ -28,31 +64,93 @@ class BrickBreaker extends FlameGame with HasCollisionDetection {
     //! add the play area component
     world.add(PlayArea());
 
-    //! add the ball component
-    /*
-    adds a new Ball instance to the world in your game. The Ball class takes velocity, position, and radius as parameters.
-    velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2):
+    playState = PlayState.welcome;
 
-    rand.nextDouble() generates a random value between 0 and 1.
-    Subtracting 0.5 centers this value around 0, giving a range from -0.5 to +0.5.
-    Multiplying by width scales this to range from -width / 2 to +width / 2.
-    For the y-component, height * 0.2 gives the ball an upward (or downward) velocity based on 20% of the screen height.
-    .normalized() ..scale(height / 4):
+    bat = Bat(
+      size: Vector2(batWidth, batHeight), // Set bat dimensions
+      cornerRadius: const Radius.circular(ballRadius / 2), // Rounded corners
+      position:
+          Vector2(width / 2, height * 0.95), // Position near bottom center
+    );
 
-    .normalized() adjusts the vector’s length to 1 while keeping its direction, making it a unit vector.
-    ..scale(height / 4) scales this unit vector to a length of height / 4. This creates a consistent speed for the ball based on the height of the screen, regardless of direction.
-    position: size / 2:
+    // debugMode = true;
+  }
 
-    size / 2 places the ball at the center of the screen, assuming size is a Vector2 representing the screen’s width and height.
-    radius: ballRadius:
+  void startGame() {
+    if (playState == PlayState.playing) return;
 
-    radius sets the size of the ball to ballRadius.
-    */
-    world.add(Ball(
+    world.removeAll(world.children.query<Ball>());
+    world.removeAll(world.children.query<Bat>());
+    world.removeAll(world.children.query<Brick>());
+
+    playState = PlayState.playing;
+    score.value = 0;
+
+    // add a Ball to the world with random horizontal velocity and upward movement
+    world.add(
+      Ball(
         velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2)
             .normalized()
-          ..scale(height / 4),
-        position: size / 2,
-        radius: ballRadius));
+          ..scale(height / 4), // Normalize and scale speed by screen height
+        position: size / 2, // Start at the center of the screen
+        radius: ballRadius, // Set ball radius
+      ),
+    );
+
+    // add a Bat to the world positioned near the bottom center of the screen
+    world.add(bat);
+
+    world.addAll([
+      // Loop through brick colors to create a grid of bricks
+      for (var i = 0; i < brickColors.length; i++) // Iterate over brick colors
+        for (var j = 1; j <= 5; j++) // Create 5 rows of bricks
+          Brick(
+            position: Vector2(
+              // Calculate x position based on column index, brick width, and gutter
+              (i + 0.5) * brickWidth + (i + 1) * brickGutter,
+              // Calculate y position based on row index, brick height, and gutter
+              (j + 2.0) * brickHeight + j * brickGutter,
+            ),
+            color: brickColors[i], // Set color for each brick
+          ),
+    ]);
   }
+
+  @override
+  void onTap() {
+    super.onTap();
+    startGame();
+  }
+
+  // Method to update the bat's position
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    // Update the bat's position based on the touch position
+    bat.position.x =
+        (bat.position.x + info.delta.global.x).clamp(0, size.x - bat.size.x);
+  }
+
+  @override
+  KeyEventResult onKeyEvent(
+      KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    super.onKeyEvent(event, keysPressed);
+    // Check which key was pressed and move the Bat accordingly
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowLeft:
+        // Move Bat left by a fixed step
+        world.children.query<Bat>().first.moveBy(-batStep);
+
+      case LogicalKeyboardKey.arrowRight:
+        // Move Bat right by a fixed step
+        world.children.query<Bat>().first.moveBy(batStep);
+
+      case LogicalKeyboardKey.space:
+      case LogicalKeyboardKey.enter:
+        startGame();
+    }
+    return KeyEventResult.handled; // Indicate that the event was handled
+  }
+
+  @override
+  Color backgroundColor() => const Color(0xfff2e8cf);
 }
--- lib/src/components/ball.dart
+++ lib/src/components/ball.dart
@@ -1,7 +1,11 @@
 import 'package:brick_breaker/src/brick_breaker.dart';
+import 'package:brick_breaker/src/components/bat.dart';
+import 'package:brick_breaker/src/components/brick.dart';
 import 'package:brick_breaker/src/components/play_area.dart';
+import 'package:brick_breaker/src/config.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flutter/material.dart';
 
 class Ball extends CircleComponent
@@ -33,7 +37,6 @@ class Ball extends CircleComponent
     super.onCollisionStart(intersectionPoints, other);
 
     // Handle collision with the PlayArea: if the ball hits the top, left, or right edges, reverse its velocity to bounce it back.
-    // If it hits the bottom edge, remove it from the game. For other collisions, log the collided object.
 
     if (other is PlayArea) {
       if (intersectionPoints.first.y <= 0) {
@@ -43,10 +46,35 @@ class Ball extends CircleComponent
       } else if (intersectionPoints.first.x >= game.width) {
         velocity.x = -velocity.x;
       } else if (intersectionPoints.first.y >= game.height) {
-        removeFromParent();
+        add(RemoveEffect(
+            delay: 0.35,
+            onComplete: () {
+              game.playState = PlayState.gameOver;
+            }));
       }
-    } else {
-      debugPrint('collision with $other');
+    } else if (other is Bat) {
+      // Reverse vertical velocity to bounce the ball upward
+      velocity.y = -velocity.y;
+
+      // Adjust horizontal velocity based on collision point with the Bat
+      velocity.x = velocity.x +
+          (position.x - other.position.x) / other.size.x * game.width * 0.3;
+    } else if (other is Brick) {
+      // Check if the ball hit the top or bottom of the brick, and reverse vertical velocity
+      if (position.y < other.position.y - other.size.y / 2) {
+        velocity.y = -velocity.y;
+      } else if (position.y > other.position.y + other.size.y / 2) {
+        velocity.y = -velocity.y;
+      }
+      // Check if the ball hit the left or right side of the brick, and reverse horizontal velocity
+      else if (position.x < other.position.x) {
+        velocity.x = -velocity.x;
+      } else if (position.x > other.position.x) {
+        velocity.x = -velocity.x;
+      }
+
+      // Apply a difficulty modifier to increase the ball's speed after hitting the brick
+      velocity.setFrom(velocity * difficultyModifier);
     }
   }
 }
